9e993f6da81b2eface690165fe20d01734608633
Fabiano Dutra <fmbdutra@gmail.com>
2019-03-20 20:59:55 -0300

    add teste validar endereço

--- ExercicioAula4QPPSW/test/exercicio/ValidatorPessoaTest.java
+++ ExercicioAula4QPPSW/test/exercicio/ValidatorPessoaTest.java
@@ -8,40 +8,63 @@ package exercicio;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author marcelo.soares
  */
 public class ValidatorPessoaTest {
-    
+
 	Pessoa pessoa;
 	ValidatorPessoa validator;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		this.validator = new ValidatorPessoa();
 		this.pessoa = new Pessoa();
 	}
+
+	@Test
+	public void testeValidaNomeOk() {
+		pessoa.setNome("teste");
+		Assert.assertTrue(validator.validar(pessoa));
+	}
+
+	@Test
+	public void testeValidaNomeErroVazio() {
+		pessoa.setNome("");
+		Assert.assertFalse(validator.validar(pessoa));
+	}
+
+	@Test
+	public void testeValidaNomeErroTamanho() {
+
+		pessoa.setNome("aaaaaaaaaa" + "aaaaaaaaaa" + "aaaaaaaaaaaaa");
+		Assert.assertFalse(validator.validar(pessoa));
+	}
+
+	@Test
+	public void testeValidaEnderecoOk() {
+		pessoa.setEndereco("Av. Aula 10, Faculdade SENAI");
+
+		assertTrue(validator.validar(pessoa));
+	}
+
+	@Test 
+    public void testeValidaEnderecoErroTamanho() {
+		pessoa.setEndereco("Avenida Aula 10, Faculdade SENAI - Porto Alegre, RS - Brasil");
+    	
+    	assertFalse(validator.validar(pessoa));
+    }
 	
-    @Test
-    public void testeValidaNomeOk(){   
-        pessoa.setNome("teste");
-        Assert.assertTrue(validator.validar(pessoa));
-    }
-    
-    @Test
-    public void testeValidaNomeErroVazio(){
-        pessoa.setNome("");
-        Assert.assertFalse(validator.validar(pessoa));
-    }
-    
-    @Test
-    public void testeValidaNomeErroTamanho(){
-        
-    	pessoa.setNome("aaaaaaaaaa"
-    			+ "aaaaaaaaaa"
-    			+ "aaaaaaaaaaaaa");
-        Assert.assertFalse(validator.validar(pessoa));
-    }    
+	@Test
+	public void testeValidaEnderecoErroVazio() {
+		pessoa.setEndereco("");
+    	
+    	assertFalse(validator.validar(pessoa));
+	}
+	
+	
+
 }
